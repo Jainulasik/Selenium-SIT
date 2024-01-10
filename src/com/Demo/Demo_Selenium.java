bc21ae88d8b1a4b5db040da5f7471d45c7bf1cd2
Jainulasik <s.jainulasik@gmail.com>
2024-01-10 22:39:04 +0530

    selenium

---
+++ src/com/Demo/Demo_Selenium.java
@@ -0,0 +1,35 @@
+package com.Demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo_Selenium {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Project\\Selenium_SIT\\Driver\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
